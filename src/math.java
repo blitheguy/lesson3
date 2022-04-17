1ed3648c7bfb58049e7a3285a86f7a2803ea4194
blitheguy <103589017+blitheguy@users.noreply.github.com>
2022-04-17 11:58:15 +0300

    Initial commit

---
+++ src/math.java
@@ -0,0 +1,24 @@
+public class math {
+    public static void main(String[] args) {
+        int a,b;
+        a = 5;
+        b = 2;
+        int sum = a+b;
+        int sub = a-b;
+        int mult = a*b;
+        float div = a/b;
+        int rem = a%b;
+        a+=6;
+        System.out.println(a);
+        a*=6;
+        System.out.println(a);
+        a++;
+        System.out.println("sum="+sum);
+        System.out.println("subtraction="+sub);
+        System.out.println("mutiplication="+mult);
+        System.out.println("division="+div);
+        System.out.println("reminder="+rem);
+        System.out.println("increment="+a);
+
+    }
+}
